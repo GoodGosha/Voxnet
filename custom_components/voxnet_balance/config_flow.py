2abcd2cc0f9c2ee7abe577fc2209dedc7b681630
GoodGosha <80222546+GoodGosha@users.noreply.github.com>
2026-04-16 15:02:42 +0300

    Add files via upload

--- custom_components/voxnet_balance/config_flow.py
+++ custom_components/voxnet_balance/config_flow.py
@@ -1,14 +1,17 @@
-import voluptuous as vol
+﻿import voluptuous as vol
 from homeassistant import config_entries
+
 from .const import DOMAIN
 
-class VoxnetConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
+class VoxnetConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-
         if user_input:
+            await self.async_set_unique_id(user_input["username"])
+            self._abort_if_unique_id_configured()
+
             return self.async_create_entry(
                 title="Voxnet",
                 data=user_input,
@@ -16,7 +19,10 @@ class VoxnetConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("username"): str,
-                vol.Required("password"): str,
-            }),
+            data_schema=vol.Schema(
+                {
+                    vol.Required("username"): str,
+                    vol.Required("password"): str,
+                }
+            ),
+        )
